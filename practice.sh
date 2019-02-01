ceccb996ca6ede0736fb099ad1673594896187c6
prateekmakhija9 <prateekmakhija9@gmail.com>
2019-01-31 17:03:27 -0700

    first

---
+++ practice.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Author : Prateek Makhija
+# Date : 1/31/2019
+# Script follows here:
+echo "Enter a number: "
+read numOne
+echo "Enter a second number:"
+read numTwo
+sum=$(($numOne + $numTwo))
+echo "The sum is : $sum"
+let prod=numOne*numTwo
+echo "The product is: $prod"
+
